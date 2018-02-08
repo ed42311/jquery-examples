fadf27f2d5b9ce7b3d1ccb6b8ede14639f733dba
ed42311 <ed42311@gmail.com>
2018-02-07 22:54:02 -0700

    all the things added

--- public/index.js
+++ public/index.js
@@ -1,1 +1,29 @@
+$(document).ready(function(){
+  $("#bruce-pic").css('border-radius', '2px');
+  $("#bruce-pic").css('border', 'darkgrey 3px solid');
+  $("#bruce-pic").css('margin-left', '20px');
+  $(".hidden-start").hide();
 
+  $("#title-text")
+  .on( "mouseenter", function() {
+      $(this).text("...My Friend");
+  })
+  .on( "mouseleave", function() {
+       $(this).text("Be Water...");
+  });
+
+  $("#empty")
+  .on( "mouseenter", function() {
+    $(this).css('background-color', 'aliceblue');
+  }).on( "mouseleave", function() {
+    $(this).css('background-color', 'white');
+  });
+
+  $("#empty").click(function(){
+    $("#cup").show().fadeOut(6500);
+  })
+
+  $("#cup").click(function(){
+    $("#crash").show().fadeOut(6500);
+  })
+})
