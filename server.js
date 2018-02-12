7f0084d1b756ba17c9d750b1eca67086d7835824
ed42311 <ed42311@gmail.com>
2018-02-12 14:39:30 -0700

    cleanup and push existing

--- server.js
+++ server.js
@@ -6,4 +6,4 @@ app.use('/', express.static(path.join(__dirname, 'public')));
 app.use('/css', express.static(__dirname + '/node_modules/bootstrap/dist/css'));
 app.use('/js/jquery', express.static(__dirname + './node_modules/jquery/dist/jquery.js'));
 
-app.listen('3000', () => console.log('working now'));
+app.listen('3000', () => console.log('working now 3000'));
